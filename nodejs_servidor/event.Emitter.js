441784514e197c9dcca33b9f1de7ae12c05ec607
Jaz <58913696+jazsnchz@users.noreply.github.com>
2020-07-27 16:22:17 -0500

    Delete event.Emitter.js

--- nodejs_servidor/event.Emitter.js
+++
@@ -1,9 +0,0 @@
-var events = require('events');
-var eventEmitter = new events.EventEmitter();
-
-// listener #1
-var listner1 = function listner1() {
-   console.log('jecucion del primer listner');
-}
-// Bind the connection event with the listner1 function
-eventEmitter.addListener('conectado', listner1);
